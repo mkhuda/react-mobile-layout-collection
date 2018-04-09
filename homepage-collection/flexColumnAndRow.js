dce3ce91351ab4d19be341c6770fccb5dd74606a
mkhuda <mhmmd.huda@gmail.com>
2018-04-09 11:10:20 +0700

    fix typos

--- homepage-collection/flexColumnAndRow.js
+++ homepage-collection/flexColumnAndRow.js
@@ -4,9 +4,9 @@ import { AppRegistry, View } from 'react-native';
 export default class FlexColumnAndRow extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'column', alignItem: 'center', justifyContent: 'center'}}>
+      <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
         <View style={{paddingLeft: 10, paddingRight: 10, height: 50, backgroundColor: 'powderblue'}}>
-          <View style={{flex: 1, flexDirection: 'column', alignItem: 'center', justifyContent: 'center'}}>
+          <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
             <View>
               <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
                 <View style={{height: 30, width: 80, backgroundColor: 'tomato'}} />
@@ -16,7 +16,7 @@ export default class FlexColumnAndRow extends Component {
           </View>
         </View>
         <View style={{paddingLeft: 10, paddingRight: 10, height: 50, backgroundColor: 'skyblue'}}>
-          <View style={{flex: 1, flexDirection: 'column', alignItem: 'center', justifyContent: 'center'}}>
+          <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
             <View>
               <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
                 <View style={{height: 30, width: 80, backgroundColor: 'tomato'}} />
